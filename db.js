0b1aaa7de65eec0bb6c988eddf184241ee1afdc5
hadiyasameer <hadiyasameer2013@gmail.com>
2025-05-13 16:29:59 +0530

    updated db.js

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
-const express=require('express');
-const mongoose=require('mongoose');
-const app=express();
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
 const dotenv = require('dotenv');
 const cors = require('cors');
 
@@ -8,19 +8,19 @@ const cors = require('cors');
 dotenv.config();
 app.use(express.json());
 app.use(cors({
-    origin: 'https://todo-frontend-two-chi.vercel.app', 
+    origin: ['https://todo-frontend-two-chi.vercel.app', 'http://localhost:5173'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  }));
-  
+}));
+
 
 mongoose.connect(process.env.MONGO_URI)
-    .then(() => console.log('connected to mongo db') )
-    .catch (err=> console.log(err))
+    .then(() => console.log('connected to mongo db'))
+    .catch(err => console.log(err))
 
-    const todoRouter=require('./routes/todoRoutes')
-    app.use('/todo',todoRouter);
+const todoRouter = require('./routes/todoRoutes')
+app.use('/todo', todoRouter);
 
-    const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log(`Server is running at port ${port}`)
