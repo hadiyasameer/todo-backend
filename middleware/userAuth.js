5e7bd55f67bbca5033fcd7bf1a461149572f40d0
hadiyasameer <hadiyasameer2013@gmail.com>
2025-05-16 04:42:32 +0530

    rename to jwt_secret in userauth

--- middleware/userAuth.js
+++ middleware/userAuth.js
@@ -2,25 +2,17 @@ import jwt from 'jsonwebtoken';
 
 export const userAuth = (req, res, next) => {
     try {
-        // Try to get the token from either cookies or Authorization header
         const user_token = req.cookies.user_token || req.header('Authorization')?.split(' ')[1];
 
         if (!user_token) {
             return res.status(401).json({ message: 'No token provided, user not authorized', success: false });
         }
-
-        // Verify the token
-        const decoded = jwt.verify(user_token, process.env.JWT_SECRET_KEY);
-
-        // Attach user data from token to the request
+        const decoded = jwt.verify(user_token, process.env.JWT_SECRET);
         req.user = decoded;
-
-        // Continue to the next middleware or route handler
         next();
 
     } catch (error) {
         console.log("Error in authentication middleware:", error);
-        // Handle different types of errors
         if (error.name === 'JsonWebTokenError') {
             return res.status(401).json({ message: 'Invalid token', success: false });
         }
